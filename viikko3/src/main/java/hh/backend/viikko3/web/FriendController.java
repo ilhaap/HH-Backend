c1ae4528a7bcddbffbf13d59317f715dd555d5e5
ihaap <194454051+ilhaap@users.noreply.github.com>
2025-09-08 00:50:10 +0300

    Tehty tehtävä Form + Result.

--- viikko3/src/main/java/hh/backend/viikko3/web/FriendController.java
+++ viikko3/src/main/java/hh/backend/viikko3/web/FriendController.java
@@ -1,5 +1,23 @@
 package hh.backend.viikko3.web;
 
-public class FriendController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hh.backend.viikko3.domain.Friend;
+
+    @Controller
+    public class FriendController {
     
-}
+        @GetMapping("/addfriend")
+        public String addFriend() {
+            return "addfriend";
+        }
+
+            @PostMapping("/save")
+        public String saveFriend(Friend friend, Model model) {
+            model.addAttribute("friend", friend);
+            return "result";
+        }
+    }
