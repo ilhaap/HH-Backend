a1a1fe832642f4f05f161e31c98a4057ad6617a7
ihaap <194454051+ilhaap@users.noreply.github.com>
2025-09-08 01:18:24 +0300

    Tehty tehtävä Form + List.

--- viikko3/src/main/java/hh/backend/viikko3/web/FriendController.java
+++ viikko3/src/main/java/hh/backend/viikko3/web/FriendController.java
@@ -1,5 +1,8 @@
 package hh.backend.viikko3.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +13,29 @@ import hh.backend.viikko3.domain.Friend;
     @Controller
     public class FriendController {
     
+        private static List<Friend> friends = new ArrayList<>();
+        static {
+            friends.add(new Friend("Seppo", "Taalasmaa"));
+            friends.add(new Friend("Ismo", "Laitela"));
+            friends.add(new Friend("Ulla", "Taalasmaa"));
+            friends.add(new Friend("Aki", "Nikkinen"));
+            friends.add(new Friend("Saku", "Salin"));
+        }
+
         @GetMapping("/addfriend")
         public String addFriend() {
             return "addfriend";
         }
 
-            @PostMapping("/save")
-        public String saveFriend(Friend friend, Model model) {
-            model.addAttribute("friend", friend);
-            return "result";
+        @PostMapping("/save")
+        public String saveFriend(Friend friend) {
+            friends.add(friend);
+            return "redirect:/friends";
         }
-    }
+
+        @GetMapping("/friends")
+        public String friendList(Model model) {
+            model.addAttribute("friends", friends);
+            return "friendlist"; 
+        }
+}
