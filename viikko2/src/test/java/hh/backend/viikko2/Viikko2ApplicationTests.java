883f59334fe699d7e6d84a9c4787452deaa30ee3
ihaap <194454051+ilhaap@users.noreply.github.com>
2025-09-07 22:47:04 +0300

    Muokattu tiedostonimiä sekä luotu viikkotehtäville 2 template.

---
+++ viikko2/src/test/java/hh/backend/viikko2/Viikko2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.backend.viikko2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viikko2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
