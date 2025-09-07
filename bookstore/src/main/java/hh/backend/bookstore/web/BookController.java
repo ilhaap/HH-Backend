0bb0e42e7fd4a7166e99f32ec891c149a266fdeb
ihaap <194454051+ilhaap@users.noreply.github.com>
2025-09-08 01:45:55 +0300

    Lisätty Bookstore -projekti.

---
+++ bookstore/src/main/java/hh/backend/bookstore/web/BookController.java
@@ -0,0 +1,13 @@
+package hh.backend.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    
+    @RequestMapping(value={"/index", "/", ""})
+    public String returnMessage() {
+        return "index";
+    }
+}
